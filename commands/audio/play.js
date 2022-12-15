f069f379b5e5ad70fa5639f24762dc0e76b7954d
d0minicS <dominic-schubert@outlook.de>
2022-12-15 20:46:30 +0100

    feat: queue added

--- commands/audio/play.js
+++ commands/audio/play.js
@@ -1,38 +1,47 @@
 const config = require(__basedir + "/config.json");
 const token = require(__basedir + "/tokens.json");
 const play = require('play-dl');
-const { 
+
+const {
     joinVoiceChannel,
     AudioPlayerStatus,
     createAudioResource,
     createAudioPlayer,
- } = require('@discordjs/voice');
- const YouTube = require("discord-youtube-api");
+} = require('@discordjs/voice');
+const YouTube = require("discord-youtube-api");
+
+var queue = new Map();
 
 module.exports = {
     name: "play",
     aliases: ["pl"],
     description: "Plays music directly from YouTube!",
-    async execute(client, Discord, message, args, audioPlayer){
+    async execute(client, Discord, message, args, audioPlayer) {
+
+        const serverQueue = queue.get(message.guild.id);
+
         console.log("command play");
         var video;
         var url;
-        if (args.length === 0){
+        if (args.length === 0) {
             return message.reply("Du hast keine URL oder Suchbegriff angegeben.");
         }
 
-        if((/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/gm).test(args[0]) == true) {
+        if ((/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/gm).test(args[0]) == true) {
             url = args[0];
         } else {
             try {
+                // TODO: play-dl search instead of youtube api
                 video = await searchYouTubeAsync(args);
                 url = video.url;
-            }catch (err) {
+
+                await queue.addQueue(url, message.guild.id);
+            } catch (err) {
                 console.log(err);
                 return message.reply("There was an error with the search!");
             }
         }
-            
+
         const voiceChannel = message.member.voice.channel;
         if (!voiceChannel) return message.reply('You are not in a voice channel!');
 
@@ -43,40 +52,80 @@ module.exports = {
             selfMute: false,
             adapterCreator: message.guild.voiceAdapterCreator
         });
-            
-        if(connection){
+
+        if (connection) {
             try {
                 console.log('Joined voice');
                 console.log("Playing with play-dl");
-                let stream = await play.stream(url);
-                let resource = createAudioResource(stream.stream, {
-                    inputType: stream.type
-                })
 
-                audioPlayer.instance = createAudioPlayer();
-                connection.subscribe(audioPlayer.instance);
+                if(!serverQueue) {
+                    const queueConst = {
+                        voiceChanne: voiceChannel.id,
+                        connection: null,
+                        songs: [],
+                        audioPlayer: null,
+                        playing: true
+                    };
 
-                audioPlayer.instance.play(resource);
+                    queue.set(message.guild.id, queueConst);
+                    queueConst.songs.push(url);
+                    queueConst.connection = connection;
+                    
+                    audioPlayer.instance = createAudioPlayer();
+                    connection.subscribe(audioPlayer.instance);
 
-                if (video){
-                    const Embed = new Discord.MessageEmbed()
-                    .setColor("#304281")
-                    .setTitle("Now Playing:")
-                    .setDescription(video.title);
-                    message.channel.send({embeds: [Embed] });
+                    queueConst.audioPlayer = audioPlayer.instance;
+                    playSong(message.guild, queueConst.songs[0]);
+                } else {
+                    serverQueue.songs.push(url)
+                    return message.channel.send(`${song.title} ist in der Queue`)
                 }
 
-                audioPlayer.instance.on(AudioPlayerStatus.Idle, () => connection.destroy());
-                audioPlayer.instance.on("error", (err) => console.log(err));
+                if (video) {
+                    const Embed = new Discord.MessageEmbed()
+                        .setColor("#304281")
+                        .setTitle("Now Playing:")
+                        .setDescription(video.title);
+                    message.channel.send({ embeds: [Embed] });
+                }
+
+                //audioPlayer.instance.on(AudioPlayerStatus.Idle, () => connection.destroy());
+                
             } catch (err) {
                 console.log(err);
             }
-        }      
+        }
     }
+}
+
+async function playSong(guild, url) {
+    const serverQueue = queue.get(guild.id);
+
+    if(!url) {
+        serverQueue.connection.destroy();
+        queue.delete(guild.id);
+        return;
+    }
+
+    let stream = await play.stream(url);
+    let resource = createAudioResource(stream.stream, {
+        inputType: stream.type
+    })
+
+    const dispatch = serverQueue.audioPlayer.play(resource)
+    .on(AudioPlayerStatus.Idle, () => {
+        serverQueue.songs.shift();
+        playSong(guild, serverQueue.songs[0]);
+    });
+
+    serverQueue.audioPlayer.on("error", (err) => console.log(err));
 }
 
 async function searchYouTubeAsync(args) {
     const youtube = new YouTube(token.ytApiKey);
     var video = await youtube.searchVideos(args.join(' '));
     return video;
-  }
+}
+
+
+
